58b8e379f0fb6d7a9defb6b3a4f20df92d74f7a3
ayalena <82506563+ayalena@users.noreply.github.com>
2021-10-04 11:53:10 +0200

    Next and previous button (only in console)

--- src/App.js
+++ src/App.js
@@ -5,21 +5,46 @@ import Pokemon from "./components/Pokemon";
 
 function App() {
     const [twenty, setTwenty] = useState([]);
+    const [offsetNumber, setOffsetNumber] = useState(0);
+    // let [nextTwenty, setNextTwenty] = useState([]);
 
     useEffect(() => {
         async function fetchTwenty() {
             try {
-                const result = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=21&offset=0`);
+                const result = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=21&offset=${offsetNumber}`);
+                console.log(result.data.results);
+                setTwenty(result.data.results);
+
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        fetchTwenty();
+    }, []);
+
+
+    useEffect(() => {
+        async function fetchNextTwenty() {
+            try {
+                const result = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=21&offset=${offsetNumber}`);
                 console.log(result.data.results);
                 setTwenty(result.data.results);
             } catch (e) {
                 console.error(e);
             }
         }
+        fetchNextTwenty();
+    }, [offsetNumber]);
 
-        fetchTwenty();
+    function handleNextTwenty() {
+        setOffsetNumber(offsetNumber + 20);
 
-    }, []);
+    }
+
+    function handlePreviousTwenty() {
+        setOffsetNumber(offsetNumber - 20);
+
+    }
 
     let twentyNames = twenty.map((pokemonName) => {
         return <Pokemon pokeName={pokemonName.name}/>;
@@ -27,9 +52,22 @@ function App() {
 
     return (
         <>
+            <button
+                type="button"
+                onClick={handlePreviousTwenty}
+            >
+                Previous
+            </button>
+            <button
+                type="button"
+                onClick={handleNextTwenty}
+            >
+                Next
+            </button>
             <div>
                 {twentyNames}
             </div>
+
         </>
     );
 }
